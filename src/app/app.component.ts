f0152663e25e693f2c1563c9646af951a4f81800
dony. <141787655+dony-aep@users.noreply.github.com>
2025-07-28 17:09:32 -0500

    Revert "# Fix: Vercel Analytics Integration for SSR v2.0.2"
    This reverts commit 689341a949a2918a4dd370566447f82504062a87.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, Inject, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { BackToTopButtonComponent } from './components/back-to-top-button/back-to-top-button.component';
@@ -19,12 +18,4 @@ import { ModalComponent } from './components/modal/modal.component';
 })
 export class AppComponent {
   title = 'anime-cover-catalog-angular';
-
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
-    if (isPlatformBrowser(this.platformId)) {
-      import('@vercel/analytics').then(({ inject: vercelInject }) => {
-        vercelInject();
-      });
-    }
-  }
 }
